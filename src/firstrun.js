7ecad054f4685dbceafc94a2dc0767a4ffc6ceee
mjuarezm <marc.juarez@kuleuven.be>
2016-01-13 00:21:17 +0100

    add first run.

---
+++ src/firstrun.js
@@ -0,0 +1,9 @@
+/* This module allows to test the extension by checking out the test branch and
+   running the tests in the test/ directory.
+ */
+/**
+ * This function is supposed to be different in the test branch.
+ */
+function setSendStats(msg) {
+    return true;
+}
